501766665b1b1085971a232c50c8198fd083caa2
Gabriel <gabriel.n.lopes@edu.senai.br>
2026-08-26 10:35:35 -0300

    feat: adiciona rota sobre ao servidor

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.send('API da Livraria no ar!');
 });
 
+app.get('/sobre', (req, res) => {
+  res.send('Livraria SENAI - Trabalho de PBE, turma 1-2026-SESI_DEV_OC_1');
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
