3d5e4720b3d9928dfd57406b5c921510d0c464cf
Gabriel <gabriel.n.lopes@edu.senai.br>
2026-09-16 08:10:39 -0300

    feat: logger

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-const express = require('express');
-const routes = require('./routes');
-const categoriaRoutes = require('./routes/categoriaRoutes');
+const express = require("express");
+const routes = require("./routes");
+const categoriaRoutes = require("./routes/categoriaRoutes");
+const logger = require("./middlewares/logger");
 
 const app = express();
 const DEFAULT_PORT = 3000;
 const PORT = process.env.PORT || DEFAULT_PORT;
 
+app.use(logger);
+app.use(routes);
+
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API da Livraria no ar!');
+app.get("/", (req, res) => {
+  res.send("API da Livraria no ar!");
 });
 
-app.get('/sobre', (req, res) => {
-  res.send('Livraria SENAI - Trabalho de PBE, turma 1-2026-SESI_DEV_OC_1');
+app.get("/sobre", (req, res) => {
+  res.send("Livraria SENAI - Trabalho de PBE, turma 1-2026-SESI_DEV_OC_1");
 });
 
 app.use(routes);
-app.use('/categorias', categoriaRoutes);
+app.use("/categorias", categoriaRoutes);
 
 if (require.main === module) {
   app.listen(PORT, () => {
